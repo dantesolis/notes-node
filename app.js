deb329ce0d0deeded896ee73b038762905b57a3d
dantesolis <dantevs@icloud.com>
2018-05-29 07:49:39 +0200

    Add yargs package to project.

--- app.js
+++ app.js
@@ -3,20 +3,30 @@ console.log(`Starting app`);
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
 // accessing args from the command line at third position;
 // argv arrays
-const command =  process.argv[2];
+const command = process.argv[2];
+
+// for passing args
+const argv = yargs.argv;
+
 console.log(`Command: ${command}`);
+console.log(`Proccess: ${process.argv}`);
+console.log(`Yargs: ${JSON.stringify(argv)}`);
 
 console.log(process.argv);
 
 if (command === 'add') {
-	console.log('Adding new note');
+	notes.addNote(argv.title, argv.body);
+
 } else if (command === 'list') {
+
 	console.log('Listing all notes');
+	
 } else if (command === 'read') {
 	console.log('Readig all notes');
 } else if (command === 'remove') {
--- notes.js
+++ notes.js
@@ -1,22 +1,11 @@
-module.exports.interval = () => {
-  setInterval(() => {
-    console.log('Starting notes.js')
-  }, 3000);
+// [TODO]: Maybe change to ES7/8 to export/import declarations?
+
+console.log('Startng notes.js');
+
+const addNotes = () => {
+  console.log(`Adding note with title and body: ${title, body}`);
 };
 
-
-module.exports.addNote = () => {
-  console.log('addNote');
-  return 'New Note';
-};
-
-// Add function
-// Make new function called add(a, b);
-// Call add funtion from app.js
-module.exports.add = (a = 0, b = 0) => {
-  if ( typeof(a || b) !== 'number' ) {
-    return 'not a number';
-  } else {
-    return a + b;
-  }
+module.exports = {
+  addNote
 };
