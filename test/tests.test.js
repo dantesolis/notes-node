2c1e0a3fec8de2499d66bb1a4915589482137249
dantesolis <dantevs@icloud.com>
2018-07-12 15:18:58 +0200

    Fix  mock test.

--- test/tests.test.js
+++ test/tests.test.js
@@ -5,8 +5,9 @@ describe('A basic test for the String object', () => {
 	describe('#split("")', () => {
 		it('should return an array containing the original elements of the string', () => {
 			const str = 'hello';
-			const splittedString = str.split('');
-			assert.equal(str.split(""), splittedString);
+			const splittedString = str.split("");
+			assert.deepStrictEqual(str.split(""), splittedString);
+			//assert.strictEqual(str.split(""), splittedString); // [ASK]: why this does not work? 
 		});
 	});
 });
